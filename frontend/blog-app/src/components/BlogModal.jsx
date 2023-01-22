d6540ab5a91ca2e04e6ef4f40c3a6b920aa73c24
root <root@DESKTOP-GSINORO.localdomain>
2023-01-22 17:37:25 +0200

    initial commit

---
+++ frontend/blog-app/src/components/BlogModal.jsx
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from "react";
+
+const BlogModal = ({ active, handleModal, token, id, setErrorMessage }) => {
+  const [title, setTitle] = useState("");
+  const [note, setNote] = useState("");
+   
+  useEffect(() => {
+    const getBlog = async () => {
+      const requestOptions = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token,
+        },
+      };
+      const response = await fetch(`/blog/${id}`, requestOptions);
+
+      if (!response.ok) {
+        setErrorMessage("Could not get the blog");
+      } else {
+        const data = await response.json();
+        setTitle(data.title);
+        setNote(data.note);
+      }
+    };
+
+    if (id) {
+      getBlog();
+    }
+  }, [id, token]);
+
+  const cleanFormData = () => {
+    setTitle("");
+    setNote("");
+	
+  };
+
+  const handleCreateBlog = async (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+	title: title,
+	note: note,
+      }),
+    };
+    const response = await fetch("/blog/", requestOptions);
+    if (!response.ok) {
+      setErrorMessage("Something went wrong when creating blog");
+    } else {
+      cleanFormData();
+      handleModal();
+    }
+  };
+
+  const handleUpdateBlog = async (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify({
+	title: title,
+        note: note,
+      }),
+    };
+    const response = await fetch(`/blog/${id}`, requestOptions);
+    if (!response.ok) {
+      setErrorMessage("Something went wrong when updating blog");
+    } else {
+      cleanFormData();
+      handleModal();
+    }
+  };
+
+  return (
+    <div className={`modal ${active && "is-active"}`}>
+      <div className="modal-background" onClick={handleModal}></div>
+      <div className="modal-card">
+        <header className="modal-card-head has-background-primary-light">
+          <h1 className="modal-card-title">
+            {id ? "Update Blog" : "Create Blog"}
+          </h1>
+        </header>
+        <section className="modal-card-body">
+          <form>
+            <div className="field">
+              <label className="label">Title</label>
+              <div className="control">
+                <input
+                  type="text"
+                  placeholder="Enter title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="input"
+                  required
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Note</label>
+              <div className="control">
+                <input
+                  type="text"
+                  placeholder="Enter note"
+                  value={note}
+                  onChange={(e) => setNote(e.target.value)}
+                  className="input"
+                />
+              </div>
+            </div>
+          </form>
+        </section>
+        <footer className="modal-card-foot has-background-primary-light">
+          {id ? (
+            <button className="button is-info" onClick={handleUpdateBlog}>
+              Update
+            </button>
+          ) : (
+            <button className="button is-primary" onClick={handleCreateBlog}>
+              Create
+            </button>
+          )}
+          <button className="button" onClick={handleModal}>
+            Cancel
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default BlogModal;
---
+++ frontend/blog-app/src/components/Table.jsx
@@ -0,0 +1,118 @@
+import React, { useContext, useEffect, useState } from "react";
+
+import ErrorMessage from "./ErrorMessage";
+import BlogModal from "./BlogModal";
+import { UserContext } from "../context/UserContext";
+
+const Table = () => {
+  const [token] = useContext(UserContext);
+  const [blogs, setBlogs] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [activeModal, setActiveModal] = useState(false);
+  const [id, setId] = useState(null);
+
+  const handleUpdate = async (id) => {
+    setId(id);
+    setActiveModal(true);
+  };
+
+  const handleDelete = async (id) => {
+    const requestOptions = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    };
+    const response = await fetch(`/blog/${id}`, requestOptions);
+    if (!response.ok) {
+      setErrorMessage("Failed to delete blog");
+    }
+
+    getBlogs();
+  };
+
+  const getBlogs = async () => {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+    };
+    const response = await fetch("/blog/", requestOptions);
+    if (!response.ok) {
+      setErrorMessage("Something went wrong. Couldn't load the blogs");
+    } else {
+      const data = await response.json();
+      setBlogs(data);
+      setLoaded(true);
+    }
+  };
+
+  useEffect(() => {
+    getBlogs();
+  }, []);
+
+  const handleModal = () => {
+    setActiveModal(!activeModal);
+    getBlogs();
+    setId(null);
+  };
+
+  return (
+    <>
+      <BlogModal
+        active={activeModal}
+        handleModal={handleModal}
+        token={token}
+        id={id}
+        setErrorMessage={setErrorMessage}
+      />
+      <button
+        className="button is-fullwidth mb-5 is-primary"
+        onClick={() => setActiveModal(true)}
+      >
+        Create Blog
+      </button>
+      <ErrorMessage message={errorMessage} />
+      {loaded && blogs ? (
+        <table className="table is-fullwidth">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Note</th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogs.map((blog) => (
+              <tr key={blog.id}>
+                <td>{blog.title}</td>
+                <td>{blog.note}</td>
+                <td>
+                  <button
+                    className="button mr-2 is-info is-light"
+                    onClick={() => handleUpdate(blog.id)}
+                  >
+                    Update
+                  </button>
+                  <button
+                    className="button mr-2 is-danger is-light"
+                    onClick={() => handleDelete(blog.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Loading</p>
+      )}
+    </>
+  );
+};
+
+export default Table;
