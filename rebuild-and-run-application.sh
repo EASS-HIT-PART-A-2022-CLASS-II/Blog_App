24840b57e281cc767469936e0ea38771e6608ebe
eladisraeli <elad.israeli@nielsen.com>
2023-01-27 01:18:45 +0200

    added rebuild and run

--- rebuild-and-run-application.sh
+++ rebuild-and-run-application.sh
@@ -1,4 +1,4 @@
 docker rm $(docker ps -qa) -f
-docker build -t backend-blog-app ./app/blog/.
+docker build -t backend-blog-app ./app/app/.
 docker build -t frontend-blog-app ./frontend/blog-app/.
 docker compose up
